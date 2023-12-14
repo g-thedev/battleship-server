4c5f3a2daad9a1c82ec97a972472c19105f734f4
Gerard Fajardo <gerard@zeta.haus>
2023-12-13 17:47:07 -0800

    Install and configure helmet and cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,29 @@
 import express, {Application, Request, Response } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 // Import routes
 import userRoutes from './routes/userRoutes';
 
+// set cors options
+const whitelist = ['http://localhost:3000', 'http://localhost:5173/'];
+const corsOptions = {
+    origin: whitelist,
+    optionsSuccessStatus: 200
+};
+
 const app: Application = express();
 
 // Middleware
+app.use(cors(corsOptions));
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// Remove once setup in production
+app.use((req, res, next) => {
+    console.log(`Request from ${req.ip} for ${req.url}`);
+    next();
+  });
 
 // Use routes
 app.use('/users', userRoutes);
