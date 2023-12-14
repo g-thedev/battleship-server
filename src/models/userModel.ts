e5e511106107d4f4f25d5951157fdab1fc6e39d9
Gerard Fajardo <gerard@zeta.haus>
2023-12-13 18:52:47 -0800

    Hash password before saving to db

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 interface IUser extends Document {
   username: string;
@@ -10,6 +11,23 @@ const UserSchema: Schema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+});
+
+// Hash password before saving to database
+UserSchema.pre('save', async function (next) {
+    const user = this as IUser;
+    if (user.isModified('password') || user.isNew) {
+        try {
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hash(user.password, salt);
+            user.password = hash;
+            next();
+        } catch (error: any) {
+            next(error);
+        }
+    } else {
+        return next();
+    }
 });
 
 export default mongoose.model<IUser>('User', UserSchema);
